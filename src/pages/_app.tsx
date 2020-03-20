ed20c0c45b8b8009548ccd787e0a6274e441bc6a
Amir Abdullin <amir.abdullin.2001@gmail.com>
2020-03-20 12:50:42 +0300

    fix/mobile loader toolbar 3

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -43,7 +43,7 @@ class App extends NextApp {
 
         return (
             <>
-                {!isDevelopment && <PreloadingScreen />}
+                {(!isDevelopment || isDevelopment) && <PreloadingScreen />}
                 <AdaptiveProvider>
                     <SliderProvider>
                         <NavbarProvider>
